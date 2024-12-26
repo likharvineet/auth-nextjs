0f4705081d60c0e405cc5f84de552543776a6082
Vineet Likhar <vineet@Vineets-Mac-mini.local>
2024-12-26 15:31:25 -0700

    import library

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,8 @@
 "use client"
+import Link from 'next/link'
+import React from 'react'
+import { useRouter } from 'next/navigation'
+import axios from 'axios'
 
 export default function SignupPage(){
     return(
