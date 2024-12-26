cb42097f2ac9748200a87530f30ed17c1c0d06bc
Vineet Likhar <vineetlikhar1998@gmail.com>
2024-12-26 15:52:41 -0700

    update: add signup form

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,13 +1,61 @@
-"use client"
-import Link from 'next/link'
-import React from 'react'
-import { useRouter } from 'next/navigation'
-import axios from 'axios'
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
-export default function SignupPage(){
-    return(
-        <div>
-            <h1>Signup</h1>
-        </div>
-    )
+export default function SignupPage() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Signup</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        name=""
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        name=""
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="Email"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="password"
+        name=""
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="Password"
+      />
+      <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        onClick={onSignup}
+      >
+        Signup
+      </button>
+      <p>
+        Already a customer? <Link href="/login">Login here</Link>
+      </p>
+    </div>
+  );
 }
