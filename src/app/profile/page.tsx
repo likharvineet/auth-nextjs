1be33dd720cb8cceb59b857f7ba94a8e6f87277b
Vineet Likhar <vineetlikhar1998@gmail.com>
2024-12-26 18:34:17 -0700

    add: logout function

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,24 @@
+"use client"
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
+    const router = useRouter()
+    const logout = async ()=>{
+        try {
+            await axios.get("/api/users/logout")
+            toast.success("Logout successful")
+            router.push("/login")
+        } catch (error: any) {
+            console.log(error.message)
+            toast.error(error.message)
+        }
+    }
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <button className="bg-blue-500 mb-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={logout}>Logout</button>
             <h1>Profile</h1>
         </div>
     );
