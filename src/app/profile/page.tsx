473c97cbda9f6027bbee3da6474f755c3fb92bdb
Vineet Likhar <vineetlikhar1998@gmail.com>
2024-12-26 19:35:38 -0700

    utilize middleware to get userdata

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,25 +1,51 @@
-"use client"
+"use client";
 import axios from "axios";
 import Link from "next/link";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function ProfilePage() {
-    const router = useRouter()
-    const logout = async ()=>{
+    const router = useRouter();
+    const [data, setData] = useState("nothing");
+    const logout = async () => {
         try {
-            await axios.get("/api/users/logout")
-            toast.success("Logout successful")
-            router.push("/login")
+            await axios.get("/api/users/logout");
+            toast.success("Logout successful");
+            router.push("/login");
         } catch (error: any) {
-            console.log(error.message)
-            toast.error(error.message)
+            console.log(error.message);
+            toast.error(error.message);
         }
-    }
+    };
+
+    const getUserDetails = async () => {
+        const res = await axios.get("/api/users/me");
+        console.log(res.data);
+        setData(res.data.data._id);
+    };
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <button className="bg-blue-500 mb-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={logout}>Logout</button>
+            <button
+                className="bg-blue-500 mb-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={logout}
+            >
+                Logout
+            </button>
             <h1>Profile</h1>
+            <button
+                className="bg-purple-500 mb-4 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+                onClick={getUserDetails}
+            >
+                Get User Details
+            </button>
+            <h2>
+                {data === "nothing" ? (
+                    "Nothing"
+                ) : (
+                    <Link href={`/profile/${data}`}>{data}</Link>
+                )}
+            </h2>
         </div>
     );
 }
